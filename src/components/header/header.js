e5a7827fe63c775acd3433e53e6638d2bed3028b
Ninh-Phan-Chinh <garapho85@gmail.com>
2020-07-29 09:44:02 +0700

    web ban hang

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class header extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default header;
