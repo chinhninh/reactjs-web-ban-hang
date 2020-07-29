5d59f194c85f3ef0f1575fe10e7b750e0b4e1a2e
Ninh-Phan-Chinh <garapho85@gmail.com>
2020-07-29 15:40:59 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header/>
       </div>
     );
   }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,79 @@
 import React, { Component } from 'react';
-import './header.css';
+import './Header.scss';
+import logo from "../../images/appIcon/ic_logo.png";
+import { FaSearch } from 'react-icons/fa';
+import { FaShoppingCart } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
 
-class header extends Component {
+class Header extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="header">
+                <div className="container">
+                    <div className="row">
+                        {/* logo */}
+                        <div className="col-1">
+                            <div className="logo">
+                                <img src={logo} />
+                            </div>
+                        </div>
+                        {/* button header */}
+                        <div className="col-5">
+                            <div className="btn-header">
+                                <ul className="list-group list-group-horizontal">
+                                    <li>
+                                        <a href="#">
+                                            Home
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Contact
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i><FaSearch /></i>
+                                            <span>Search</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i><FaShoppingCart /></i>
+                                            <span>Cart</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        {/* right header */}
+                        <div className="col-6">
+                            <div className="right-header">
+                                <ul className="list-group list-group-horizontal">
+                                    <li>
+                                        <a href="#">
+                                            <i><FaUser /></i>
+                                            <span>Phan Chinh Ninh</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Login
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Register
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default header;
+export default Header;
